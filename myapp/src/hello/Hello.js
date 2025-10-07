39296e0d3723e93ae6df671c03f251947dedb929
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2025-10-07 16:03:09 +0100

    create my first component

---
+++ myapp/src/hello/Hello.js
@@ -0,0 +1,22 @@
+//  export default function Hello() {
+//      return (
+//          <>
+//              <h1>hello mohamed in your app</h1>
+//              <p>hello moha,med </p>
+//         </>
+//     )
+// }
+import React from "react";
+
+
+export default class Hello extends React.Component{
+    render() {
+        return (
+            <>
+                <h1>hello mohamed in your app</h1>
+            </>
+        )
+    }
+    
+
+}
