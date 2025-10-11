b651d53ec69adc25e130430762e925f09c433d2d
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2025-10-11 15:20:33 +0100

    react hooks Useeffect

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './Form/Form'
-import Fruitslists from './fruite/FruitsList';
 import Counter from './Counter/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Counter></Counter>
+     <Counter initialValue='0'></Counter>
   </React.StrictMode>
 );
 
